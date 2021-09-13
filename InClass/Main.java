3ee76028142654f35c5d39584273d6f6d9721974
codytheking <codytheking93@gmail.com>
2021-09-13 11:40:32 -0700

    In class example

---
+++ InClass/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 10;
+        int b = 5;
+        double diff = sub(a, b);
+        System.out.println(a + " - " + b + " = " + diff);
+    }
+
+    public static double sub(double m, int n) {
+        double diff = m - n;
+        return diff;
+    }
+}
+
+
