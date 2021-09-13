47421e3d8957ae72fb73eecc8ff1db505f08cf38
codytheking <codytheking93@gmail.com>
2021-09-13 11:36:30 -0700

    Starter code

---
+++ Main.java
@@ -0,0 +1,43 @@
+
+/**
+ * This class has a method for converting currency.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        // Print welcome message (1 line)
+        
+
+        // Prompt for input of USD (1 line)
+        
+        
+        // Take input for USD (1 line)
+        
+        
+        // Convert USD to DOGE by calling method and 
+        // storing value in a variable (1 line)
+        
+        
+        // Print the result (1 line)
+    }
+    
+    /**
+     * Converts USD to Dogecoin. Note: 1 USD = 3.8983 DOGE
+     * 
+     * @param  usd  the amount of US Dollars to be converted.
+     * @return      the amount of Dogecoin converted from argument. 
+     */
+    // write method heading here...
+    {
+        // your implementation...
+    }
+}
